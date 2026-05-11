7f1b2703cf2773f24036a320fcd86261cb8e5246
Mitali <105365766+Kayleexx@users.noreply.github.com>
2026-05-12 03:11:37 +0530

    Change default SSH user and host configuration

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ use crate::ssh::Host;
 
 pub fn default_host() -> Host {
     Host {
-        user: "kaylee".into(),
-        host: "192.168.0.47".into(),
+        user: "my_name".into(),
+        host: "my_server_ip".into(),
     }
 }
