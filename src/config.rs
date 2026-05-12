0216c8b6a10b34f5ea825e0039dd7d745e619745
Kayleexx <sinhamitali56@gmail.com>
2026-05-12 10:55:08 +0530

    feat: add toml based configuration system

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,43 @@
+use anyhow::Result;
+use serde::Deserialize;
+
 use crate::ssh::Host;
 
-pub fn default_host() -> Host {
-    Host {
-        user: "my_name".into(),
-        host: "my_server_ip".into(),
-    }
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub host: HostConfig,
+    pub network: NetworkConfig,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct HostConfig {
+    pub ip: String,
+    pub user: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct NetworkConfig {
+    pub domain: String,
+}
+
+pub fn load_config() -> Result<Config> {
+    let path = format!(
+        "{}/.config/tsuki/config.toml",
+        std::env::var("HOME")?
+    );
+
+    let content = std::fs::read_to_string(path)?;
+
+    let config: Config = toml::from_str(&content)?;
+
+    Ok(config)
+}
+
+pub fn default_host() -> Result<Host> {
+    let config = load_config()?;
+
+    Ok(Host {
+        user: config.host.user,
+        host: config.host.ip,
+    })
 }
